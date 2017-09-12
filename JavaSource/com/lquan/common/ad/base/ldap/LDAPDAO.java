bbff79022035b71fd228253e7074e6530d501ab3
刘劝 <1570599338@qq.com>
2017-09-12 17:19:43 +0800

    base

---
+++ JavaSource/com/lquan/common/ad/base/ldap/LDAPDAO.java
@@ -0,0 +1,161 @@
+package com.lquan.common.ad.base.ldap;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.naming.AuthenticationException;
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+
+import com.lquan.util.LDAPUtil;
+
+
+/**
+ * 验证Ad域的登陆Dao
+ * @author liuquan
+ *
+ */
+public class LDAPDAO {
+	/*String userName = "liuquan";//AD域认证，用户的登录UserName
+    String password = "M@x201611";//AD域认证，用户的登录PassWord
+    
+    String host = "192.168.1.230";//AD域IP，必须填写正确
+    String domain = "@mi.local";//域名后缀，例.@noker.cn.com
+    String port = "389"; //端口，一般默认389
+    String url = new String("ldap://" + host + ":" + port);//固定写法
+    String user = userName.indexOf(domain)>0?userName:userName + domain;//网上有别的方法，但是在我这儿都不好使，建议这么使用
+*/	
+	private DirContext ctx = null;
+	// AD域的IP
+	private static String host;
+	// 域名后缀
+	private String domain;
+	// 端口
+	private static String port="389"; //端口，一般默认389;
+	private static Hashtable<Object, String> env = new Hashtable();//实例化一个Env
+	static {
+		env.put(Context.SECURITY_AUTHENTICATION, "simple");//LDAP访问安全级别(none,simple,strong),一种模式，这么写就行
+		env.put(Context.INITIAL_CONTEXT_FACTORY,"com.sun.jndi.ldap.LdapCtxFactory");// LDAP工厂类
+	}
+	
+	/**
+	 * 此处是获取连接AD域的 DirContext，注意调用此方法后不再使用后一定要关闭改连接
+	 * @param userName 用户名
+	 * @param password 密码
+	 * @return
+	 * @throws Exception 
+	 */
+	public  Map<String,Object> getDirContext(String userName,String password) throws Exception{
+		String url = new String("ldap://" + host + ":" + port);
+		String user = userName.indexOf(domain) > 0 ? userName : userName + domain;
+        env.put(Context.SECURITY_PRINCIPAL, user); //用户名
+        env.put(Context.SECURITY_CREDENTIALS, password);//密码
+        env.put(Context.PROVIDER_URL, url);//Url
+        Map<String,Object> map = new HashMap<String, Object>();
+      /*  try {*/
+        ctx = new InitialDirContext(env);// 初始化上下文
+        System.out.println("身份验证成功!");
+        SearchControls searchCtls = new SearchControls();  
+        searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        // 设置过滤
+        String searchFilter = "(&(objectClass=user)(name="+userName+"))";
+        String searchBase = "DC=MI,DC=local";
+        String[] returnedAtts = { "description","name","mail","employeeID","userpassword"};
+        searchCtls.setReturningAttributes(returnedAtts);  
+        NamingEnumeration<SearchResult> answer = ctx.search(searchBase, searchFilter,searchCtls); 
+        while (answer.hasMoreElements()) {  
+            SearchResult sr = (SearchResult) answer.next();
+            // 获取属性
+            String mail = LDAPUtil.getUser(sr, "mail");// 电子邮件
+            String name = LDAPUtil.getUser(sr, "description"); // 姓名
+            String loginName = LDAPUtil.getUser(sr, "name");// 登录名称
+            map.put("mail", mail);
+            map.put("name", name);
+            map.put("loginName", loginName);
+        }
+        
+        
+        ctx.close();
+        /*} catch (AuthenticationException e) {
+            System.out.println("身份验证失败!");
+            e.printStackTrace();
+        } catch (javax.naming.CommunicationException e) {
+        	 System.out.println("AD域连接失败!");
+             e.printStackTrace();
+        }  catch (Exception e) {
+             //System.out.println("身份验证未知异常!");
+             e.printStackTrace();
+         }*/
+		return map;
+	}
+
+	
+	/**
+	 * 此方法时关闭对应AD域的连接
+	 */
+	public void close(){
+		try {
+			ctx.close();
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+            if(null!=ctx){
+                try {
+                    ctx.close();
+                    ctx=null;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+       }
+	}
+	
+	
+
+	public DirContext getCtx() {
+		return ctx;
+	}
+
+
+	public void setCtx(DirContext ctx) {
+		this.ctx = ctx;
+	}
+
+
+	public String getHost() {
+		return host;
+	}
+
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+
+	public String getDomain() {
+		return domain;
+	}
+
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+
+	public String getPort() {
+		return port;
+	}
+
+
+	public void setPort(String port) {
+		this.port = port;
+	}
+
+}
---
+++ JavaSource/com/lquan/common/mail/EmailSender.java
@@ -0,0 +1,172 @@
+package com.lquan.common.mail;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 邮件发送类 简单实现，用于发送邮件 基本配置在props/mail.properties
+ * 
+ * @author lquan
+ */
+public class EmailSender {
+
+	private Log log = LogFactory.getLog(EmailSender.class);
+	
+	private JavaMailSenderImpl mailSender;
+	
+	private SimpleMailMessage simpleMailMessage;
+	// 线程池
+	private ThreadPoolTaskExecutor taskExecutor;
+
+
+	/**
+	 * 内部发送邮件线程类
+	 * @author liuquan
+	 */
+	private class SendMailThread implements Runnable {
+		private String to; // 接受邮件
+		private String cc; //　抄送
+		private String bcc; // 密送
+		private String subject;
+		private String content;
+
+		private SendMailThread(String to,String cc,String bcc,String subject, String content) {
+			super();
+			this.to = to;
+			this.cc = cc;
+			this.bcc = bcc;
+			this.subject = subject;
+			this.content = content;
+		}
+
+		@Override
+		public void run() {
+			sendSimpleTextEmail(to,cc,bcc, subject, content);
+		}
+	}
+
+	/**
+	 * 利用线程池发送简单文本的邮件
+	 * @param to
+	 *            接收邮箱
+	 * @param cc
+	 * 			     抄送邮箱
+	 * @param bcc
+	 * 			     密送邮箱
+	 * @param subject
+	 *            邮件主题
+	 * @param content
+	 *            邮件内容
+	 * @return
+	 */
+	public boolean send(String to,String cc,String bcc, String subject, String content) {
+
+		try {
+			taskExecutor.execute(new SendMailThread(to,cc,bcc,subject, content));
+		} catch (MailException e) {
+			log.error("发送邮件错误", e);
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * 	 发送简单文本的邮件
+	 * 
+	 * @param to
+	 *            接收邮箱
+	 * @param subject
+	 *            邮件主题
+	 * @param content
+	 *            邮件内容
+	 * @return
+	 * @param to
+	 * @param cc
+	 * @param bcc
+	 * @param subject
+	 * @param content
+	 */
+	public void sendSimpleTextEmail(String to,String cc,String bcc, String subject, String content) {
+		
+		if(to != null && !"".equals(to.trim())){
+			simpleMailMessage.setTo(emailAddressToArray(to)); // 发送
+			if(cc != null && !"".equals(cc.trim()))
+				simpleMailMessage.setCc(emailAddressToArray(cc)); // 抄送
+			if(bcc != null && !"".equals(bcc.trim()))
+				simpleMailMessage.setBcc(emailAddressToArray(bcc)); //  密送
+			simpleMailMessage.setSubject(subject);
+			simpleMailMessage.setText(content);
+	
+			mailSender.send(simpleMailMessage);
+		}else
+			log.info("EmailSender 发送邮件出错……没有发送人");
+	}
+	
+	/**
+	 * 转换邮件接收人的格式为数组类型
+	 * 
+	 * @param emails
+	 * 
+	 * @return
+	 */
+	private String[] emailAddressToArray(String emailStr){
+		log.info("邮箱转换前的数据："+ emailStr);
+		int firstPostion = emailStr.indexOf(";");
+		int lastPostion = emailStr.lastIndexOf(";");
+		int strLength = emailStr.length();
+		String[] emailAddress=null;
+		
+		// 去掉头部的“;”
+		if(firstPostion==0 && strLength>1){
+			emailStr = emailStr.substring(1, strLength);
+		}
+		
+		// 去掉尾部的";"
+		if(strLength==lastPostion+1 && lastPostion>1){
+			emailStr = emailStr.substring(0, strLength-1);
+			
+		}
+		emailAddress = emailStr.split(";");
+		log.info("*************转换-Start***********");
+		for(int i=0;i<emailAddress.length;i++){
+			log.info(emailAddress[i]);
+		}
+		log.info("**************--End***************");
+		return emailAddress;
+	}
+	
+	public JavaMailSenderImpl getMailSender() {
+		return mailSender;
+	}
+
+	public void setMailSender(JavaMailSenderImpl mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public SimpleMailMessage getSimpleMailMessage() {
+		return simpleMailMessage;
+	}
+
+	public void setSimpleMailMessage(SimpleMailMessage simpleMailMessage) {
+		this.simpleMailMessage = simpleMailMessage;
+	}
+
+	public ThreadPoolTaskExecutor getTaskExecutor() {
+		return taskExecutor;
+	}
+
+	public void setTaskExecutor(ThreadPoolTaskExecutor taskExecutor) {
+		this.taskExecutor = taskExecutor;
+	}
+
+	
+	
+}
+
